be5ddb0c00fa1aaaf64b37ce1fde0ae30ff30654
cello-j <cello-j@klab.com>
2015-03-19 19:05:53 +0900

    fix welcome page

--- app/views/user/welcome.php
+++ app/views/user/welcome.php
@@ -1,13 +1,12 @@
 <!--<a class="btn btn-inverse" href="/">Home</a>
 <a class="btn btn-inverse" href="<?php eh(url('user/logout')) ?>">Logout</a>
 -->
-	 <h1>Member List </h1>
-
 
 <div class="comment">
     <div class="meta">
         <?php foreach ($userlist as $k => $v): ?>
-        <?php eh($k + 1) ?>: <?php eh($v['username']);  ?>
+            <h3><?php eh($v['username']);  ?>さん、こんにちは！</h3>
+            <hr />
     </div>
 </div>
 <?php endforeach ?>
